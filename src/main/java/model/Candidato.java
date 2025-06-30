d7fdd6e8e20ab1b5a78509b11b98d21edb038b13
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-30 11:46:08 -0300

    Implementação da model Candidato

---
+++ src/main/java/model/Candidato.java
@@ -0,0 +1,29 @@
+package model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "TB_CANDIDATO")
+public class Candidato {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Nome da cidade é obrigatório")
+    @Column(nullable = false)
+    private String cidade;
+
+    @NotNull(message = "Nome da estado é obrigatório")
+    @Column(nullable = false)
+    private String estado;
+
+    @NotNull(message = "Nome da país é obrigatório")
+    @Column(nullable = false)
+    private String pais;
+}
