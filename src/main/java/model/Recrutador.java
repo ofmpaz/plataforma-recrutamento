85daa15617d289eef6429d25762f099793a2cce4
Deise Andrade <deise_andrade_paz@hotmail.com>
2025-06-30 11:40:29 -0300

    Implementação da model Recrutador

--- src/main/java/model/Recrutador.java
+++ src/main/java/model/Recrutador.java
@@ -1,4 +1,20 @@
 package model;
 
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Recrutador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "Empresa é obrigatório")
+    @Column(nullable=false)
+    String empresa;
 }
